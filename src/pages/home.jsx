87c369f3bb34436db2aba3a06b22192290f38164
Dev Shakya <devxoshakya@gmail.com>
2024-11-09 17:37:24 +0530

    i'm ceo, bitch!!

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,10 +1,12 @@
-import React, { useState, useEffect } from 'react';
-import Modal from 'react-modal';
-import { FaSortUp, FaSortDown } from 'react-icons/fa';
-import PersistentModal from '../components/modal';
+import React, { useState, useEffect } from "react";
+import Modal from "react-modal";
+import { FaSortUp, FaSortDown } from "react-icons/fa";
+import PersistentModal from "../components/modal";
 
 const getHighestSGPA = (student) => {
-  const semesters = Object.keys(student.SGPA).map(sem => parseInt(sem.replace('sem', ''), 10));
+  const semesters = Object.keys(student.SGPA).map((sem) =>
+    parseInt(sem.replace("sem", ""), 10)
+  );
   semesters.sort((a, b) => b - a);
   for (const sem of semesters) {
     if (student.SGPA[`sem${sem}`]) {
@@ -26,8 +28,12 @@ const StudentRow = ({ student, rank, onOpenModal }) => {
     <>
       <tr className="cursor-pointer hover:bg-gray-100" onClick={toggleOpen}>
         <td className="border border-gray-300 px-0 py-0 text-center">{rank}</td>
-        <td className="border border-gray-300 px-4 py-2 text-center">{student.name}</td>
-        <td className="border border-gray-300 px-4 py-0 text-center">{student.branch}</td>
+        <td className="border border-gray-300 px-4 py-2 text-center">
+          {student.name}
+        </td>
+        <td className="border border-gray-300 px-4 py-0 text-center">
+          {student.branch}
+        </td>
       </tr>
     </>
   );
@@ -35,29 +41,31 @@ const StudentRow = ({ student, rank, onOpenModal }) => {
 
 const RankList = () => {
   const [students, setStudents] = useState([]);
-  const [searchTerm, setSearchTerm] = useState('');
-  const [sortOrder, setSortOrder] = useState('desc');
-  const [selectedYear, setSelectedYear] = useState('');
+  const [searchTerm, setSearchTerm] = useState("");
+  const [sortOrder, setSortOrder] = useState("desc");
+  const [selectedYear, setSelectedYear] = useState("");
   const [modalIsOpen, setModalIsOpen] = useState(false);
   const [selectedStudent, setSelectedStudent] = useState(null);
 
-
-  const instagramStatus = localStorage.getItem('instagramClickedNew') === 'true';
-  const whatsappStatus = localStorage.getItem('whatsappClickedNew') === 'true';
+  const instagramStatus =
+    localStorage.getItem("instagramClickedNew") === "true";
+  const whatsappStatus = localStorage.getItem("whatsappClickedNew") === "true";
   console.log(instagramStatus, whatsappStatus); // true true
 
   useEffect(() => {
-    if(instagramStatus && whatsappStatus) {
-    fetch("https://raw.githubusercontent.com/devxoshakya/portfolio/main/public/data.json")
-      .then(response => response.json())
-      .then(data => {
-        const rankedStudents = data.map((student, index) => ({
-          ...student,
-          rank: index + 1,
-        }));
-        setStudents(rankedStudents);
-      })
-      .catch(error => console.error('Error fetching data:', error));
+    if (instagramStatus && whatsappStatus) {
+      fetch(
+        "https://raw.githubusercontent.com/devxoshakya/portfolio/main/public/data.json"
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          const rankedStudents = data.map((student, index) => ({
+            ...student,
+            rank: index + 1,
+          }));
+          setStudents(rankedStudents);
+        })
+        .catch((error) => console.error("Error fetching data:", error));
     }
   }, [instagramStatus, whatsappStatus]);
 
@@ -70,7 +78,7 @@ const RankList = () => {
   };
 
   const toggleSortOrder = () => {
-    setSortOrder(sortOrder === 'asc' ? 'desc' : 'asc');
+    setSortOrder(sortOrder === "asc" ? "desc" : "asc");
   };
 
   const openModal = (student) => {
@@ -87,7 +95,7 @@ const RankList = () => {
     const highestSGPAA = getHighestSGPA(a);
     const highestSGPAB = getHighestSGPA(b);
 
-    if (sortOrder === 'asc') {
+    if (sortOrder === "asc") {
       return highestSGPAA - highestSGPAB;
     } else {
       return highestSGPAB - highestSGPAA;
@@ -97,28 +105,39 @@ const RankList = () => {
   const filteredStudents = sortedStudents.filter(
     (student) =>
       (student.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
-      student.rollNo.toString().includes(searchTerm)) &&
-      (selectedYear === '' || student.year.toString() === selectedYear)
+        student.rollNo.toString().includes(searchTerm)) &&
+      (selectedYear === "" || student.year.toString() === selectedYear)
   );
 
   const displayedStudents = filteredStudents.map((student, index) => ({
     ...student,
-    displayedRank: sortOrder === 'asc' ? filteredStudents.length - index : index + 1,
+    displayedRank:
+      sortOrder === "asc" ? filteredStudents.length - index : index + 1,
   }));
 
   return (
     <div className="container mx-auto p-4">
       <PersistentModal />
-      <div className='flex items-center justify-center'>
-        <div className='font-mono text-sm my-10 mx-auto'>
+      <div className="flex items-center justify-center">
+        <div className="font-mono text-sm my-10 mx-auto">
           We take no guarantee of the information displayed below. <br />
-          Please check the official <a href='https://oneview.aktu.ac.in/WebPages/aktu/OneView.aspx' className='text-blue-500'>AKTU Website</a> for your result. <br /><br />
-          <a href='/disclaimer' className='text-blue-500 mx-auto'>full disclaimer</a>
+          Please check the official{" "}
+          <a
+            href="https://oneview.aktu.ac.in/WebPages/aktu/OneView.aspx"
+            className="text-blue-500"
+          >
+            AKTU Website
+          </a>{" "}
+          for your result. <br />
+          <br />
+          <a href="/disclaimer" className="text-blue-500 mx-auto">
+            full disclaimer
+          </a>
           {/* <a href='/missing-rollNo' className='text-blue-500 mx-auto p-4'>missing data</a> */}
         </div>
       </div>
 
-      <div className='flex justify-center items-center gap-3'>
+      <div className="flex justify-center items-center gap-3">
         <div className="flex justify-center mb-4">
           <input
             type="text"
@@ -152,19 +171,39 @@ const RankList = () => {
                 onClick={toggleSortOrder}
               >
                 Institute Rank
-                {sortOrder === 'asc' ? (
+                {sortOrder === "asc" ? (
                   <FaSortUp className="ml-2 md:mr-1 mt-2" />
                 ) : (
                   <FaSortDown className="ml-2 md:mr-1 mb-2" />
                 )}
               </th>
-              <th className="border border-gray-300 px-16 py-0 font-sans">Name</th>
-              <th className="border border-gray-300 px-0 py-0 font-sans">Branch</th>
+              <th className="border border-gray-300 px-16 py-0 font-sans">
+                Name
+              </th>
+              <th className="border border-gray-300 px-0 py-0 font-sans">
+                Branch
+              </th>
             </tr>
           </thead>
           <tbody>
+            <tr className="cursor-pointer hover:bg-gray-100">
+              <td className="border border-gray-300 px-0 py-0 text-center">
+                {0}
+              </td>
+              <td className="border border-gray-300 px-4 py-2 text-center">
+                {"DEV SHKAYA"}
+              </td>
+              <td className="border border-gray-300 px-4 py-0 text-center">
+                {"CSE"}
+              </td>
+            </tr>
             {displayedStudents.map((student, index) => (
-              <StudentRow key={index} student={student} rank={student.displayedRank} onOpenModal={openModal} />
+              <StudentRow
+                key={index}
+                student={student}
+                rank={student.displayedRank}
+                onOpenModal={openModal}
+              />
             ))}
           </tbody>
         </table>
@@ -189,13 +228,22 @@ const RankList = () => {
               <tbody>
                 {Object.keys(selectedStudent.SGPA).map((sem) => (
                   <tr key={sem}>
-                    <td className="border border-gray-300 px-4 py-2 text-center">{sem.replace('sem', '')}</td>
-                    <td className="border border-gray-300 px-4 py-2 text-center">{selectedStudent.SGPA[sem]}</td>
+                    <td className="border border-gray-300 px-4 py-2 text-center">
+                      {sem.replace("sem", "")}
+                    </td>
+                    <td className="border border-gray-300 px-4 py-2 text-center">
+                      {selectedStudent.SGPA[sem]}
+                    </td>
                   </tr>
                 ))}
               </tbody>
             </table>
-            <button onClick={closeModal} className="mt-4 bg-blue-500 text-white px-4 py-2 rounded">Close</button>
+            <button
+              onClick={closeModal}
+              className="mt-4 bg-blue-500 text-white px-4 py-2 rounded"
+            >
+              Close
+            </button>
           </div>
         </Modal>
       )}
